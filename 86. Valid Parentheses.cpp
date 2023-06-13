b0470f30c3afa0c7adddc5d8aa47bcac43c9416c
Shailesh <85133291+Shailesh003@users.noreply.github.com>
2023-06-13 20:21:19 +0530

    Add files via upload

---
+++ 86. Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+bool isValidParenthesis(string s)
+{
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='{'|| s[i]=='['|| s[i]=='('){
+                st.push(s[i]);
+            }
+            else if(s[i]=='}'){
+                if(!st.empty()&& st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(s[i]==']'){
+                if(!st.empty() && st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+             else if(s[i]==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();    // Write your code here.
+}
