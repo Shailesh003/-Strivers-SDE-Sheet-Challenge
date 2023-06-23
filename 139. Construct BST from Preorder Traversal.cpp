59a9e21a265a9ece2b82774c824f841a578f3aeb
Shailesh <85133291+Shailesh003@users.noreply.github.com>
2023-06-23 23:04:36 +0530

    Add files via upload

---
+++ 139. Construct BST from Preorder Traversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+
+    class TreeNode{
+    public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        ~TreeNode() {
+            if (left){
+                delete left;
+            }
+            if (right){
+                delete right;
+            }
+        }
+    };
+
+*************************************************************/
+TreeNode<int>* build(vector<int> &preOrder,int &i,int bound){
+    if(i==preOrder.size()|| preOrder[i]>bound)return NULL;
+    TreeNode<int>* root=new TreeNode<int>(preOrder[i++]);
+    root->left=build(preOrder,i,root->data);
+    root->right=build(preOrder,i,bound);
+    return root;
+}
+TreeNode<int>* preOrderTree(vector<int> &preOrder){
+    int i=0;
+   return build(preOrder,i,INT_MAX);
+}
